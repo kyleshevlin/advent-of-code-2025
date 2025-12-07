7e32f620002b7e182b5b331579796b48f2255791
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2025-12-07 10:13:24 -0800

    solution for day 6

--- src/day-06/index.js
+++ src/day-06/index.js
@@ -1,12 +1,89 @@
-import { getInput } from '../utils'
+import { getInput, sum, product } from '../utils'
 
 const data = getInput(__dirname)
 
-function formatInput(input) {}
+const OP_TO_FN = {
+  '+': sum,
+  '*': product,
+}
 
-export function solution1() {}
+function formatInput(input) {
+  return input.trim().split('\n')
+}
 
-export function solution2(input) {}
+export function solution1(input) {
+  const rows = formatInput(input)
+
+  const groups = []
+  for (const row of rows) {
+    const cols = row.trim().split(' ').filter(Boolean)
+
+    for (let i = 0; i < cols.length; i++) {
+      if (!groups[i]) groups[i] = []
+      groups[i].push(cols[i])
+    }
+  }
+
+  const problems = groups.map(group => {
+    const op = group.pop()
+    return { op, nums: group.map(Number) }
+  })
+
+  const results = problems.map(problem => {
+    const { op, nums } = problem
+    return OP_TO_FN[op](nums)
+  })
+
+  return sum(results)
+}
+
+export function solution2(input) {
+  const rows = formatInput(input)
+  // Find the longest row, we're going to need to pad shorter ones to match
+  const longestRow = Math.max(...rows.map(row => row.length))
+  const paddedRows = rows.map(row => row.padEnd(longestRow, ' '))
+
+  // Pop the last row, it contains the operators
+  const rawOps = paddedRows.pop()
+  const ops = rawOps.split(' ').filter(Boolean)
+
+  // Gather all the characters vertically
+  const cols = []
+  for (let i = 0; i < paddedRows[0].length; i++) {
+    const col = []
+    for (const row of paddedRows) {
+      col.push(row[i])
+    }
+
+    cols.push(col.join(''))
+  }
+
+  // We need to group the numbers together for each problem
+  // The divider is an item of all spaces
+  const groups = []
+  let idx = 0
+  for (const col of cols) {
+    if (col.trim() === '') {
+      idx++
+      continue
+    }
+
+    if (!groups[idx]) groups[idx] = []
+    groups[idx].push(col)
+  }
+
+  const problems = groups.map((group, idx) => {
+    const op = ops[idx]
+    return { op, nums: group.map(Number) }
+  })
+
+  const results = problems.map(problem => {
+    const { op, nums } = problem
+    return OP_TO_FN[op](nums)
+  })
+
+  return sum(results)
+}
 
 // console.log(solution1(data))
 // console.log(solution2(data))
--- src/day-06/index.test.js
+++ src/day-06/index.test.js
@@ -1,11 +1,16 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   + 
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(4277556)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(3263827)
 })
